7f2e21e9facaa0cd258b1e2915d6a782358c1c8a
Md. Sifatullah <sifatullah35-617@diu.edu.bd>
2024-07-15 04:18:15 +0600

    bye

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3564694908"
+    serviceWorkerVersion: "645684561"
   }
 });
