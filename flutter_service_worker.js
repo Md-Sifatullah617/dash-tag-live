6485238dff06c40e28170e070c6c1e605045fd05
Md. Sifatullah <sifatullah35-617@diu.edu.bd>
2024-07-28 17:56:15 +0600

    new

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,8 +4,8 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"main.dart.js": "8f39d440ced5bec0fbb56c8af74b8811",
-"flutter_bootstrap.js": "7da1b51ea6f55b750a4a0e8d84eacaff",
+"main.dart.js": "1e0e7b402518ecfe8af3a66de414324c",
+"flutter_bootstrap.js": "4ec2e5a5666ac67eefa9621ae5e57486",
 "version.json": "93c265047baebf0d0ee3706a0c1d1275",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
@@ -1589,8 +1589,8 @@ const RESOURCES = {"flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
 "assets/fonts/Caveat-VariableFont_wght.ttf": "9bd9bde59dc816ef93cd18069b65a40e",
 "assets/fonts/Rajdhani-Bold.ttf": "62eb2a35acdf719d19b2598e8e5f69df",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"index.html": "0c9ac43221412a9cff4cf0c269d9b3aa",
-"/": "0c9ac43221412a9cff4cf0c269d9b3aa"};
+"index.html": "6a3c18992d50aceb73d0b11df0cd2b1d",
+"/": "6a3c18992d50aceb73d0b11df0cd2b1d"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
