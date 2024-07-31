6cabed893aa6eabd82bf80c2dbf920a0b7f4ce5e
Md. Sifatullah <sifatullah35-617@diu.edu.bd>
2024-07-31 12:58:35 +0600

    kldfa

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,8 +4,8 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"main.dart.js": "341284683a9afcda6f11c90548d09d60",
-"flutter_bootstrap.js": "8cf0a6d0ed50a299b81ccef07a2c5792",
+"main.dart.js": "046aeac5a69b19fd7c8bc0d6d1a3d850",
+"flutter_bootstrap.js": "d0405779df85ef792952356fa0420ed7",
 "version.json": "93c265047baebf0d0ee3706a0c1d1275",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
